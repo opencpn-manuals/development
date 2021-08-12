fc344a33e0a8260ea3cbe743cc8afa3e3a09a6a4
Alec Leamas <leamas.alec@nowhere.nt>
2021-08-12 13:23:58 +0200

    Fix broken links, update linkchecker script [full-linkcheck]

--- ci/w3c-linkcheck.sh
+++ ci/w3c-linkcheck.sh
@@ -2,20 +2,9 @@
 
 subject=$(git log -1 --oneline)
 if [[ "$subject" != *full?linkcheck* ]]; then
-    echo "No full-linkcheck annotaion found. Exiting."
+    echo "No [full-linkcheck] annotation found. Exiting."
     exit 0
 fi
 sudo apt install -y w3c-linkchecker
 
-if checklink  -qb --depth 3 \
-    --exclude 'https://www.opencpn.org.*' \
-    --exclude 'https://opencpn.org/wiki/dokuwiki/doku.php.*' \
-    --exclude 'https://opencpn.org/flyspray.*' \
-    docs/index.html &> linkcheck.log
-then
-    cat linkcheck.log
-else
-    tail -20 linkcheck.log
-    paste_link=$(curl -F 'f:1=<-' ix.io < linkcheck.log)
-    echo "Complete linkchecker log is at $paste_link"
-fi
+checklink --exclude opencpn.org  --depth 4 -s docs/index.html
