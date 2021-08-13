7d78a0229bb8f537a39b7b632401039d30ea64ae
Alec Leamas <leamas.alec@nowhere.nt>
2021-08-13 11:18:58 +0200

    Links:: trying to hide some false positives [full-linkcheck]

--- ci/w3c-linkcheck.sh
+++ ci/w3c-linkcheck.sh
@@ -5,9 +5,14 @@ if [[ "$subject" != *full?linkcheck* ]]; then
     echo "No [full-linkcheck] annotation found. Exiting."
     exit 0
 fi
-sudo apt install -y w3c-linkchecker
+sudo apt install -qq  -y w3c-linkchecker
 
-checklink --exclude opencpn.org \
-          --exclude bigdumboat.com \
-          --exclude cubian.org \
-          --depth 4 -s docs/index.html
+checklink \
+    --suppress-broken '403:https://www.opencpn.org/' \
+    --suppress-broken '403:https://opencpn.org/' \
+    --suppress-broken '403:https://www.opencpn.org/flyspray/' \
+    --suppress-broken '403:https://opencpn.org/flyspray/' \
+    --suppress-broken '403:$dokuwiki?id=opencpn:opencpn_user_manual' \
+    --suppress-redirect 'https://opencpn-manuals.github.io/development->https://opencpn-manuals.github.io/development/' \
+    --exclude 'https://opencpn.org/wiki/dokuwiki/' \
+    --depth 4 -s docs/index.html
